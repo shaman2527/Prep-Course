af169b8050658dbd2fd994f7cc4a98781c743a88
master <playa8229@gmail.com>
2022-07-29 00:40:31 -0400

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,19 +4,20 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 32;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
+
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40;
+const nuevaMultiplicacion = 10 * 40 === 400;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -27,7 +28,10 @@ const nuevoModulo = 21 % 5 === null;
 
 function devolverString(str) {
     // "Return" la string provista: str
-    // Tu código:
+    // Tu código: 
+    if (str === str) {
+        return str;
+    }
 
 }
 
@@ -35,31 +39,48 @@ function suma(x, y) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código:
-
+    var sumando = x + y;
+    return sumando;
 }
+
+
+
 
 function resta(x, y) {
     // Resta "y" de "x" y devuelve el valor
     // Tu código:
-
+    let resta = x - y;
+    return resta;
 }
 
 function multiplica(x, y) {
     // Multiplica "x" por "y" y devuelve el valor
     // Tu código:
 
+    let mulpli = x * y;
+    return mulpli;
+
+
 }
 
 function divide(x, y) {
     // Divide "x" entre "y" y devuelve el valor
     // Tu código:
-
+    let divido = x / y;
+    return divido;
 }
 
 function sonIguales(x, y) {
     // Devuelve "true" si "x" e "y" son iguales
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (x == y) {
+        return true;
+    } else {
+        return false;
+    }
+
+
 
 }
 
@@ -67,6 +88,14 @@ function tienenMismaLongitud(str1, str2) {
     // Devuelve "true" si las dos strings tienen la misma longitud
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (str1.length === str2.length) {
+        return true;
+    } else {
+        return false;
+
+    }
+
+
 
 }
 
